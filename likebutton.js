6fa05cdf99f69f6db501bf78ff4922b93dea3c54
rm-mplpl-ebi <66986303+rm-mplpl-ebi@users.noreply.github.com>
2020-10-13 16:09:39 +0900

    Update likebutton.js

--- likebutton.js
+++ likebutton.js
@@ -1,17 +1,17 @@
 var animateButton = function(e) {
 
-  e.preventDefault;
-  //reset animation
-  e.target.classList.remove('animate');
-  
-  e.target.classList.add('animate');
-  setTimeout(function(){
+    e.preventDefault;
+    //reset animation
     e.target.classList.remove('animate');
-  },700);
-};
-
-var bubblyButtons = document.getElementsByClassName("hero-btn");
-
-for (var i = 0; i < bubblyButtons.length; i++) {
-  bubblyButtons[i].addEventListener('click', animateButton, false);
-}
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName('.hero-btn');
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
